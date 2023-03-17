a14fc674f3f4f7630ca3f1c14fb6bbda2e431aac
asdfadfzcv <yunjun556@gmail.com>
2023-03-17 10:32:24 +0900

    asd

--- js/common.js
+++ js/common.js
@@ -19,13 +19,4 @@ $(function () {
   $(".gotop").click(function () {
     $("html").animate({ scrollTop: 0 }, 600)
   })
-
-  // 필요시 사용 //
-
-  // $(".btn4").click(function () {
-  //   var introBox = $(".intro_box")
-  //   if (!introBox.is(":animated")) {
-  //     introBox.stop().animate({ opacity: "toggle", height: "toggle" }, 800)
-  //   }
-  // })
 })
